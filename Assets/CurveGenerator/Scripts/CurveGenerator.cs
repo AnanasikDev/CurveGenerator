4bac4958efe622c5b493c50e12af63cb84444b02
Олег Бровченков <oleg.brovchenkov@gmail.com>
2021-10-28 21:43:46 +0300

    Init

---
+++ Assets/CurveGenerator/Scripts/CurveGenerator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public sealed class GraphGenerator : MonoBehaviour
+{
+    [SerializeField] private AnimationCurve BaseGraph;
+    public float I = 0;
+    [SerializeField] private float Speed = 0.05f;
+
+    public static GraphGenerator instance { get; private set; }
+
+    private void Awake() => instance = this;
+    public float MoveNext()
+    {
+        I += Speed;
+
+        return BaseGraph.Evaluate(I);
+    }
+}
---
+++ Assets/CurveGenerator/Scripts/CurveVisualizer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+public class GraphVisualizer : MonoBehaviour
+{
+    [SerializeField] private AnimationCurve VisualizationCurve = new AnimationCurve();
+    [SerializeField] private float TimeTick = 0.04f;
+    private void Start()
+    {
+        InvokeRepeating("Visualize", TimeTick, TimeTick);
+    }
+    private void Visualize()
+    {
+        float a = GraphGenerator.instance.MoveNext();
+
+        VisualizationCurve.AddKey(GraphGenerator.instance.I, a);
+    }
+}
